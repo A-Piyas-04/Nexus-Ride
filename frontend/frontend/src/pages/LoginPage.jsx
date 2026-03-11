d2da9b31d51f4f79a751b2ee1050583ddd9a4f01
A-Piyas-04 <ahnafpias@iut-dhaka.edu>
2026-03-11 12:14:44 +0600

    .

--- frontend/frontend/src/pages/LoginPage.jsx
+++ frontend/frontend/src/pages/LoginPage.jsx
@@ -140,9 +140,6 @@ export default function LoginPage() {
           <div className="space-y-2">
             <div className="flex items-center justify-between">
               <Label htmlFor="password">Password</Label>
-              <Link to="/forgot-password" className="text-sm text-primary-600 hover:underline">
-                Forgot password?
-              </Link>
             </div>
             <Input
               id="password"
